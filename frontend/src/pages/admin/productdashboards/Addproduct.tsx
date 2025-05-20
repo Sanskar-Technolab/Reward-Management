bf296b807cd7c927c1e877a6f903a5cfef164a5e
Mini Soni <mini@sanskartechnolab.com>
2025-05-20 13:09:50 +0530

    add validation for reward points

--- frontend/src/pages/admin/productdashboards/Addproduct.tsx
+++ frontend/src/pages/admin/productdashboards/Addproduct.tsx
@@ -123,11 +123,17 @@ const AddProduct: React.FC = () => {
         }
     };
 
+    // const isValidNumber = (title: string) => {
+    //     // Regex to allow only numbers
+    //     const regex = /^[0-9]+$/;
+    //     return regex.test(title);
+    // };
     const isValidNumber = (title: string) => {
-        // Regex to allow only numbers
-        const regex = /^[0-9]+$/;
+        // Regex to allow integers and floats like 123, 0.001, .5, 123.
+        const regex = /^(\d+(\.\d*)?|\.\d+)$/;
         return regex.test(title);
     };
+    
 
     const addProduct = async (fileUrls: string[]) => {
         const missingFields: string[] = [];
